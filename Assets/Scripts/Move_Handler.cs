62f07816a82207bae52083268766fb5735ae3107
Gitter1000 <tuev.mikhail@gmail.com>
2025-01-07 16:52:52 -0800

    Added Move class and expanded character move implementation

--- Assets/Scripts/Move_Handler.cs
+++ Assets/Scripts/Move_Handler.cs
@@ -1,12 +1,28 @@
 using UnityEngine;
+using System;
 
 public class MoveHandler : MonoBehaviour
 {
     private int baseDamage;
 
-    public int calcDamage(int power, Pokemon attacker, Pokemon defender)
+    public int calcDamage(Move move, Pokemon attacker, Pokemon defender)
     {
-        baseDamage = power * (attacker.getAttack() / defender.getDefense());
+        System.Random rand = new System.Random();
+        int randNum = rand.Next(1, 101);
+
+        if(randNum <= move.getAccuracy()) //The move is successful
+        {
+            baseDamage = move.getBasePower() * (attacker.getAttack() / defender.getDefense());
+        }
+        else //The move is unsuccessful
+        {
+            baseDamage = 0;
+        }
+
+        //Decrement power points for the attacker for the move used
+        int newPowerPoints = attacker.getMove(attacker.getType(), move.getName()).getPowerPoints();
+        attacker.updatePowerPoints(attacker.getType(), move.getName(), newPowerPoints);
+        
         return baseDamage;
     }
 }
